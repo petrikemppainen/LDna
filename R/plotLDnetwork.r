64970245ac82bb39b2014ba6dbcc1b8c04a8429e
Petri Kemppainen <petrikemppainen2@gmail.com>
2014-03-25 20:09:30 +0100

    update to documentation

--- R/plotLDnetwork.r
+++ R/plotLDnetwork.r
@@ -1,17 +1,17 @@
 #' Plots linkage disequilibrium networks
 #'
-#' Allows for visual inspection of clusters extracted by \code{\link{extractClusters}}.
+#' Allows for visual inspection of clusters identified by \code{\link{extractClusters}}.
 #'
 #' @keywords plotLDnetwork
 #' @param ldna Output from \code{\link{LDnaRaw}}
 #' @param el Edge list of all pairwise LD values for instance produced by \code{\link{mat2el}}
 #' @param option \code{option=1} prints a full LD network from and edge list (\code{el}) and a specified LD threshold (\code{threshold}). \code{option=2} prints LD networks based on the files \code{clusters} and \code{summary} that contains information of extracted clusters.
 #' @param threshold Specifies the LD threshold at which an LD network is plotted. Only required for option=1.
-#' @param clusters A list of vectors giving loci for clusters extracted by \code{\link{extractClusters}}.
-#' @param summary Summary information of the clusters created by \code{\link{summaryLDna}} that contains.
+#' @param clusters Output from \code{\link{extractClusters}}.
+#' @param summary Output from \code{\link{summaryLDna}}.
 #' @param exl A list of locus names to be excluded from the LD networks (default is \code{NULL}).
-#' @param full.network If \code{TRUE} (default), includes all clusters in the LD networks.
-#' @param include.parent If \code{full.network} is set to \code{FALSE}, \code{include.parent} allows one to see any loci/clusters a given focal cluster merges with (with decreaisn LD threshold). When \code{FALSE}, only the focal cluster is shown.
+#' @param full.network If \code{TRUE} (default), includes all loci in the LD networks, not recommended for large data sets.
+#' @param include.parent If \code{full.network=FALSE} and \code{include.parent=TRUE} all loci from the parent cluster (after merger) are included. If \code{include.parent=FALSE} only the focal cluster is shown.
 #' @param after.merger Whether to show LD networks at an LD threshold just before (\code{FALSE}) or just after (\code{TRUE}) merger.
 #' @seealso \code{\link{LDnaRaw}}, \code{\link{extractClusters}}, \code{\link{mat2el}} and \code{\link{summaryLDna}}
 #' @export
@@ -19,21 +19,24 @@
 #' @examples
 #' ### Example with option=1
 #' data(LDna)
-#' el <- mat2el(r2.baimaii_subs)
 #' plotLDnetwork(el=el, option=1, threshold=0.4)
 #' ### Examples with option 2
 #' par(mfcol=c(1,2))
 #' ldna <- LDnaRaw(r2.baimaii_subs)
 #' clusters <- extractClusters(ldna, min.edges=20, phi=5)
 #' summary <- summaryLDna(ldna, clusters, r2.baimaii_subs)
+#' el <- mat2el(r2.baimaii_subs)
 #' #default settings with option=2
 #' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary)
-#' # Examples with three useful settings (option=2)
-#' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=FALSE, include.parent=FALSE, after.merger=TRUE) 
+#' ## Other useful settings
+#' # For large data sets
+#' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=FALSE, include.parent=FALSE, after.merger=FALSE)
+#' # To visualise the merger
+#' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=TRUE, after.merger=FALSE)
+#' # Or
 #' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=FALSE, include.parent=TRUE, after.merger=TRUE)
+#' # To show that ususally several clusters are involved in most mergers
 #' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=FALSE, include.parent=TRUE, after.merger=FALSE)
-#' plotLDnetwork(ldna, el, option=2, clusters=clusters, summary=summary, full.network=TRUE, after.merger=TRUE)
-
 
 plotLDnetwork <- function(ldna, el, option, threshold, clusters, summary,
 exl=NULL, full.network=TRUE, include.parent=FALSE, after.merger=FALSE){
@@ -41,6 +44,7 @@ exl=NULL, full.network=TRUE, include.parent=FALSE, after.merger=FALSE){
     if(option==2) option2(ldna, el, clusters, summary, exl, full.network, include.parent, after.merger)
     if(option==1) return(g)
 }
+
 option1 <- function(el, threshold, exl){
     # exlude loci
     el <-el[!(el[,1] %in% exl),]
@@ -67,6 +71,7 @@ option2 <- function(ldna, el, clusters, summary, exl, full.network, include.pare
         names(clusters)[[i]])
     }
 }
+
 option2raw <- function(ldna, el, exl, loci, col, full.network, threshold, include.parent, after.merger, cluster.name){
     if(!is.null(exl)){
         el <- el[!(el[,1] %in% exl),]
--- R/r2.baimaii_subs.R
+++ R/r2.baimaii_subs.R
@@ -1,6 +1,6 @@
 #' @name r2.baimaii_subs
-#' @title Matrix of pairwise r-squared values
-#' @description Upper diagonal pairwise matrix of r-squared values between 957 SNPs from 91 wild-caught individuals of the SE Asian mosquito malaria vector, \emph{Anopheles baimaii}.
+#' @title Matrix of pairwise \eqn{R^2} values
+#' @description Upper diagonal pairwise matrix of \eqn{R^2} values between 957 SNPs from 91 wild-caught individuals of the SE Asian mosquito malaria vector, \emph{Anopheles baimaii}.
 #' @docType data
 #' @usage r2.baimaii_subs
 #' @author Petri Kemppainen \email{petrikemppainen2@@gmail.com}
