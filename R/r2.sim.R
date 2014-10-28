7e91604f35fee510ac1b3edee9b72cbf34929a6f
Petri Kemppainen <petrikemppainen2@gmail.com>
2014-10-28 10:21:57 +0100

    added data for tutorial

---
+++ R/r2.sim.R
@@ -0,0 +1,7 @@
+#' @name r2.sim
+#' @title List of five r-squared matrices from simulated data
+#' @description Simulated data sets. More info to come.
+#' @docType data
+#' @usage r2.sim
+#' @author Petri Kemppainen \email{petrikemppainen2@@gmail.com}
+NULL
