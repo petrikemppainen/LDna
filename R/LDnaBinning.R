0345679e3be3caddea4020c8793490427d58f7b2
Petri Kemppainen <p3taimen@gmail.com>
2018-04-10 13:49:42 +0300

    Bug fixes and documentation update

--- R/LDnaBinning.R
+++ R/LDnaBinning.R
@@ -1,8 +1,8 @@
-#' Linkage disequilibrium (LD) network binning
+#' Linkage disequilibrium (LD) network clustering
 #'
-#' Finds clusters of loci connected by high LD within a sliding window summerizes genetic variation using principal component analyses
+#' Finds clusters of loci connected by high LD within non-overlapping windows and summerises this information using Principal Component Analysis, to be used in downstream Genome Wide Association (GWA) analyses.
 #' 
-#' If \code{plot.tree} and \code{plot.graph} are set to \code{TRUE}, \code{extractClusters} plots two graphs (default). The first shows all \eqn{\lambda}-values oredered from low to high and indicates which values are above \eqn{\lambda_{lim}}. Values corresponding to \emph{"selected outlier clusters", SOCs} are indicated in red and values corresponding to \emph{COCs} are indiced in blue. A \emph{COC} is defined as any \emph{OC} that contains loci from an \emph{OC} already extracted at a higher LD threshold. The second graph gives the tree illustrating cluster merger with decreasing LD threshold where nodes represent and node distance gives the LD threholds at which these events occur. Branches corresponding to \emph{SOCs} are indicated in red and those corresponding to \emph{COCs} are indiced in blue (if \code{rm.COCs=FALSE}).
+#' Uses complete linkage clustering within non-overlapping windows of SNPs to find clusters of SNPs connected by high LD. Window breakpoints are placed where r^2 between adjacent SNPs within a window (defined by \code{w1}) is below \code{LD_threshold1}. 
 #'
 #' @param snp A matrix with individuals as rows and bi-allelic SNP genotypes as columns. SNPs must be coded as 0,1 or 2 for the number of alleles of the reference nucleotide.
 #' @param mapsnp A matrix with each row correspoinding a column in \code{snp}, column one corresponding to chromosome or linkage group and column two corresponding to physical position in the genome.
@@ -12,15 +12,34 @@
 #' @param LD_threshold1 Minimum LD value within a cluster
 #' @param LD_threshold2 Minimum median LD within each cluster, and for the second step of clustering
 #' @param PC_threshold Minimum cummulative amount of genetic variation explained by extracted PCs
-#' @param verbose More detailed information of progress is outputted
-#' @param plot.tree Plot tree. Default \code{FALSE}
-#' @param mc.cores number of cores for mclapply
+#' @param verbose More detailed information of progress is printed on screen
+#' @param plot.tree Plot complete linkage tree. 
+#' @param mc.cores Number of cores for mclapply
 #' @param plot.network File name for plotting network. If \code{NULL} (default) no network is plotted.
 #' @param threshold_net Threshold for edges when plotting network.
 #' @keywords LDnB
 #' @seealso \code{\link{LDnaRaw}}, \code{\link{summaryLDna}} and \code{\link{extractClusters}}
 #' @author Petri Kemppainen \email{petrikemppainen2@@gmail.com}, zitong.li
-#' @return Returns a list of three objects. \code{map_cl} contains most of the relvant informaton for each cluster, inluding summary statistics (Median LD, MAD etc).\cr The second object, \code{PC_cl} includes a matrix of individuals (rows) and PCs (columns) corresponding to each row in map_pc. This is what you use for subsequent GWAS analyses.\cr The third object contains a list of locus names (each entry corresponding to a row in map_pc) 
+#' @return Returns a list of three objects. \code{map_cl} is a data frame that contains most of the relevant information for each cluster, inluding summary statistics (Median LD, MAD etc, see below). 
+#' \code{PC_cl} includes a matrix of individuals (rows) and PCs (columns) corresponding to each row in map_pc. This is what you use for subsequent GWA analyses.
+#' The third object (\code{cl_snps} contains a list of locus indexes (each entry corresponding to a row in map_pc for entries where PC=1).\cr
+#' \cr
+#' The columns in file \code{map_cl} are:
+#' \item{Chr}{Chromosome or LG identifer}
+#' \item{Window}{Window identifier, recycled among chromosomes)}
+#' \item{Pos}{Mean position of SNPs in a cluster}
+#' \item{Min}{Most downstream position of SNPs in a cluster}
+#' \item{Max}{Most upstream position of SNPs in a cluster}
+#' \item{Range}{Max-Min}
+#' \item{nSNPs}{Number of SNPs in the cluster}
+#' \item{Median}{Median pair wise LD between loci in a cluster}
+#' \item{MAD}{Median absolute deviation among pair wise LD-values between loci in a cluster}
+#' \item{PC}{PC identifier (first PC explaining most of the genetic variation)}
+#' \item{PVE}{Cummulative proportion of variation explained by each PC}
+#' \item{Grp}{Unique group identifier for PCs extracted from each cluster. Used by the emmax group algorithm.}
+#' @references Kemppainen, P., Knight, C. G., Sarma, D. K., Hlaing, T., Prakash, A., Maung Maung, Y. N., … Walton, C. (2015). Linkage disequilibrium network analysis (LDna) gives a global view of chromosomal inversions, local adaptation and geographic structure. Molecular Ecology Resources, 15(5), 1031–1045. https://doi.org/10.1111/1755-0998.12369\cr
+#' \cr
+#' Li, Z., Kemppainen, P., Rastas, P., Merila, J. Linkage disequilibrium clustering-based approach for association mapping with tightly linked genome-wide data. Accepted to Molecular Ecology Resources.
 #' @examples
 #' # To come
 
@@ -35,51 +54,47 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
   #for each chromosome
   # i <- 12
   for(i in unique(mapsnp[,1])){
-    cat('Finding windows \n')
-    snp.pos <- snp.pos.org[mapsnp[,1]==i]
+    cat("Finding windows \n")
+    snp.pos <- snp.pos.org[mapsnp[, 1] == i]
+    snp.id.chr <- snp.id[mapsnp[, 1] == i]
+    nL <- length(snp.pos)
     
-    temp1 <- 10^max(unlist(lapply(strsplit(as.character(snp.pos), '.', fixed = TRUE), nchar)))
+    gds_name <- paste0(gsub(" ", "", Sys.time()), ".gds")
     
-    snp.pos <- unlist(lapply(unique(snp.pos), function(x){
-      temp <- snp.pos[snp.pos==x]
-      
-      
-      temp+signif(((1:length(temp)/length(temp))-1/length(temp))/temp1
-                  , log10(temp1)+2)
-    }))
-    
-    gds_name <- paste0(gsub(' ', '', Sys.time()), '.gds')
-    
-    snpgdsCreateGeno(gds_name, genmat = snp[,mapsnp[,1]==i] , sample.id = 1: nrow(snp) , snp.pos = snp.id[mapsnp[,1]==i], snpfirstdim=FALSE) ### sample ID should be 1: how many individuals you have
+    snpgdsCreateGeno(gds_name, genmat = snp[, mapsnp[, 1] == i], sample.id = 1:nrow(snp), snp.pos = snp.id.chr, snpfirstdim = FALSE)
     file <- snpgdsOpen(gds_name)
-    
-    ## get hotspots
-    LDmat <- snpgdsLDMat(file, method="r",slide=1, verbose=FALSE)$LD^2
-    
+    LDmat <- snpgdsLDMat(file, method = "r", slide = 1, verbose = FALSE)$LD^2
+    nL-length(unlist(Windows))
     snpgdsClose(file)
-    system(paste0('rm ', gds_name))
-    
-    nWindows <- as.integer(length(snp.pos)/nSNPs)
-    
-    #plot(slideFunct(LDmat[1,], w1))
+    system(paste0("rm ", gds_name))
     
     
-    if(nWindows>1){
-      temp <- which(slideFunct(LDmat[1,], w1)<LD_threshold1)
+    nWindows <- as.integer(nL/nSNPs)
+    
+    
+    if (nWindows > 1) {
+      temp <- which(slideFunct(LDmat[1, ], w1) < LD_threshold1)
       
-      hotspots <- sapply(seq(1, length(snp.pos), length.out = nWindows+1)[-1], function(i){
-        which.min(abs(i-temp))
-      })
-      hotspots <- c(0, temp[hotspots])
+      hotspots <- sapply(seq(1, nL, length.out = nWindows + 
+                               1), function(i) {
+                                 which.min(abs(i - temp))
+                               })
       
-      Windows <- lapply(1:(length(hotspots)-1), function(x){
-        snp.id[mapsnp[,1]==i][(hotspots[x]+1):(hotspots[x+1])]
+      hotspots <- temp[hotspots]
+      hotspots[length(hotspots)] <- nL
+      hotspots[1] <- 0
+      
+      Windows <- lapply(1:(length(hotspots) - 1), function(x) {
+        snp.id.chr[(hotspots[x]+1):(hotspots[x + 1])]
       })
-    }else{
-      Windows <- list(snp.id[mapsnp[,1]==i])
+      
+      
+    }else {
+      Windows <- list(snp.id.chr)
     }
+    cat(paste0("Number of windows: ", length(Windows), "; window  sizes: ", 
+               paste(sapply(Windows, length), collapse = ":"), "\n"))
     
-    cat(paste0('Number of windows: ', length(Windows), '; window  sizes: ', paste(sapply(Windows, length), collapse=':'), '\n'))
     
     
     ## note that we initially called clusters for bins in the below code and have not changed this.
@@ -91,23 +106,30 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
       snp_bin  <- snp[,which(snp.id%in%Window)]
       
       
-      snpgdsCreateGeno(paste0(b,"_data2.gds"), genmat = snp_bin, sample.id = 1: nrow(snp) , snp.id = snp.id.bin, snpfirstdim=FALSE) ### sample ID should be 1: how many individuals you have
-      file <- snpgdsOpen(paste0(b,"_data2.gds"))
+      snpgdsCreateGeno(paste0(b,gds_name), genmat = snp_bin, sample.id = 1: nrow(snp) , snp.id = snp.id.bin, snpfirstdim=FALSE) ### sample ID should be 1: how many individuals you have
+      file <- snpgdsOpen(paste0(b,gds_name))
       
       # estimate LD within a sliding window
       if(w2>length(snp.id.bin)) w2 = -1
       
       MAT <- snpgdsLDMat(file, method="r",slide=w2, verbose=FALSE)
       snpgdsClose(file)
-      system(paste0('rm ', paste0(b,"_data2.gds")))
+      system(paste0('rm ', paste0(b,gds_name)))
       
-      LDmat <- data.table(remove=rep(NA, length(MAT$snp.id)))
-      for(x in 1:(length(MAT$snp.id))){
-        LDmat[(1:w2+x)[!is.nan(MAT$LD[,x])],as.character(x) := MAT$LD[!is.nan(MAT$LD[,x]),x]^2]
+      
+      if(w2!=-1){
+        LDmat <- data.table(remove=rep(NA, length(MAT$snp.id)))
+        for(x in 1:(length(MAT$snp.id))){
+          LDmat[(1:w2+x)[!is.nan(MAT$LD[,x])],as.character(x) := MAT$LD[!is.nan(MAT$LD[,x]),x]^2]
+        }
+        LDmat <- as.matrix(LDmat)
+        LDmat <- LDmat[,-1]
+      }else{
+        for(x in 1:(length(MAT$snp.id))){
+          LDmat <- MAT$LD^2
+        }
       }
       
-      LDmat <- as.matrix(LDmat)
-      LDmat <- LDmat[,-1]
       LDmat[is.na(LDmat)]<- 0
       LDmat[upper.tri(LDmat)] <- NA
       diag(LDmat) <- NA
@@ -182,7 +204,7 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
             
             temp <- ldna2$clusterfile[,colnames(ldna2$clusterfile) %in% names(clusters2)]
             
-            ## remove any SOCs if its parent SOC also meets threshold requirements
+            ## remove any SOCs if its parent COC also meets threshold requirements
             clusters2 <- clusters2[!names(clusters2) %in% colnames(temp)[which(nested2=='COC', arr.ind = T)[,2]]]
           }
           
@@ -207,19 +229,6 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
       if(!is.null(plot.network)){
         ## plot network 
         cat('plotting network \n')
-        col_vector <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "#F0027F", 
-                        "#BF5B17", "#666666", "#1B9E77", "#D95F02", "#7570B3", "#E7298A", 
-                        "#66A61E", "#E6AB02", "#A6761D", "#666666", "#A6CEE3", "#1F78B4", 
-                        "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00", 
-                        "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928", "#FBB4AE", "#B3CDE3", 
-                        "#CCEBC5", "#DECBE4", "#FED9A6", "#FFFFCC", "#E5D8BD", "#FDDAEC", 
-                        "#F2F2F2", "#B3E2CD", "#FDCDAC", "#CBD5E8", "#F4CAE4", "#E6F5C9", 
-                        "#FFF2AE", "#F1E2CC", "#CCCCCC", "#E41A1C", "#377EB8", "#4DAF4A", 
-                        "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999", 
-                        "#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854", "#FFD92F", 
-                        "#E5C494", "#B3B3B3", "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072", 
-                        "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5", "#D9D9D9", "#BC80BD", 
-                        "#CCEBC5", "#FFED6F")
         
         nClust <- length(which(sapply(clusters, length)>1))
         
@@ -236,8 +245,6 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
           ifelse(sapply(clusters, length)[x]==1, return('black'),  return(rep(temp2[1:nClust][x], sapply(clusters, length)[x])))
         }))[match(colnames(LDmat), unlist(clusters) )]
         
-        #plot.netork <- 'network'
-        #threshold_net <- 0.9
         png(paste0(paste(plot.network, i, b, sep='_'), '.png'), res = 300, width = 6, height=6, units = 'in')
         plotLDnetwork(ldna,LDmat,option=1, threshold=threshold_net, col=Col, frame.color = frame.col)
         dev.off()
@@ -245,7 +252,7 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
       
       
       
-      ## extract as many PCs from each cluster as necessary to explain 80% of the variation
+      ## extract as many PCs from each cluster as necessary to explain 'PC_threshold' of the variation
       
       if(verbose) cat('Extracting PCs \n')
       
@@ -268,8 +275,6 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
                     1-sum(as.numeric(names(temp2))*temp2)/length(unlist(clusters))
                   , clusters=clusters, PCs=PCs, stats=stats))
     }, mc.cores=mc.cores)
-    
-    #, mc.cores=mc.cores)}
   }
   
   ### prepare output
@@ -315,9 +320,7 @@ LDnClustering <- function(snp, mapsnp, nSNPs=1000, w1=10, w2=100 ,LD_threshold1 
     })
   }))
   
-  # chromosome <- out[[1]]
-  # window <- chromosome[[1]]
-  # x <- window[[2]][[1]]
+
   Pos <-  unlist(sapply(out, function(chromosome){
     sapply(chromosome, function(window){
       rep(sapply(window[[2]], function(x) mean(mapsnp[as.numeric(x),2])), sapply(window[[3]], function(bin) ifelse(is.matrix(bin), ncol(bin), 1)))
@@ -401,3 +404,17 @@ flatten <- function(x) {
   if (!inherits(x, "list")) return(list(x))
   else return(unlist(c(lapply(x, flatten)), recursive = FALSE))
 }
+
+col_vector <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#386CB0", "#F0027F", 
+                "#BF5B17", "#666666", "#1B9E77", "#D95F02", "#7570B3", "#E7298A", 
+                "#66A61E", "#E6AB02", "#A6761D", "#666666", "#A6CEE3", "#1F78B4", 
+                "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00", 
+                "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928", "#FBB4AE", "#B3CDE3", 
+                "#CCEBC5", "#DECBE4", "#FED9A6", "#FFFFCC", "#E5D8BD", "#FDDAEC", 
+                "#F2F2F2", "#B3E2CD", "#FDCDAC", "#CBD5E8", "#F4CAE4", "#E6F5C9", 
+                "#FFF2AE", "#F1E2CC", "#CCCCCC", "#E41A1C", "#377EB8", "#4DAF4A", 
+                "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999", 
+                "#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854", "#FFD92F", 
+                "#E5C494", "#B3B3B3", "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072", 
+                "#80B1D3", "#FDB462", "#B3DE69", "#FCCDE5", "#D9D9D9", "#BC80BD", 
+                "#CCEBC5", "#FFED6F")
